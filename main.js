160324fc32fc4455f3372e544d3a55d22dea62fe
JosueRoddy <96269163+RoddyAnchundia@users.noreply.github.com>
2022-09-14 23:20:21 -0500

    act

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const button = document.querySelector('#menu-button'); // Hamburger Icon
 const menu = document.querySelector('#menu'); // Menu
 
+
+// Desplegar menú
 button.addEventListener('click', () => {
   menu.classList.toggle('hidden');
 });
